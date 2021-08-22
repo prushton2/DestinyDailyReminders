7fce58dcdb7dcb40e5d8b98b7a44f02cc86591e9
prushton2 <peterrushton52@gmail.com>
2021-08-22 17:25:36 -0400

    built function to check collections and compare

--- comparecollections.py
+++ comparecollections.py
@@ -6,14 +6,38 @@ import os
 jsm = __import__("jsonmanager")
 jsonmanager = jsm.JsonManager(os.path.realpath(os.path.join(os.path.dirname(__file__), "config.json")))
 apiKey = jsonmanager.load()["apiKey"]
+test = jsm.JsonManager(os.path.realpath(os.path.join(os.path.dirname(__file__), "test.json")))
+
 
 getVendors = __import__("getvendors")
 
-def compareCollections(userInformation):
+def compareCollections(userInformation, mods):
+
+    dictionary_url = "https://raw.githubusercontent.com/prushton2/DestinyCollections/master/dict.json" #Grabs the dictionary from github
+    res = requests.get(dictionary_url)
+    dictionary = json.loads(res.text)
     
-    membershipType, destinyMembershipId, characterId = userInformation[0][0], userInformation[0][1], userInformation[0][2]
+    HEADERS=  {
+        "X-API-Key": apiKey
+    }
 
-    vendorInfo = getVendors.getVendorData(membershipType, destinyMembershipId, characterId)
+    membershipType, destinyMembershipId, characterId = userInformation[0], userInformation[1], userInformation[2]
 
-    for i in userInformation:
-        membershipType, destinyMembershipId, characterId = userInformation[i][0], userInformation[i][1], userInformation[i][2]
+    url = f"https://bungie.net/Platform/Destiny2/{membershipType}/Profile/{destinyMembershipId}/?components=800"
+
+    res = requests.get(url, headers=HEADERS)
+    
+    test.save(json.loads(res.text))
+
+    collections = json.loads(res.text)["Response"]["profileCollectibles"]["data"]["collectibles"]
+
+    itemsUserDoesntHave = []
+
+    for i in mods:
+        if(collections[i]["state"]%2 == 1): #read if user DOESNT have item
+            itemsUserDoesntHave.append(i)
+
+    for i, item in enumerate(itemsUserDoesntHave):
+        itemsUserDoesntHave[i] = dictionary[item]
+    
+    return itemsUserDoesntHave
