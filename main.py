462d62636010dd92fc4c7ddd67e23b1a3eaf378a
prushton2 <peterrushton52@gmail.com>
2021-08-24 00:25:09 -0400

    added a command to register users

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import pafy
 
 from discord.ext import commands
 from discord.ext import tasks
+
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
+
 import os
 import colorama
 import time
@@ -16,7 +20,8 @@ gv = __import__("getvendors")
 config = jsm.JsonManager(os.path.realpath(os.path.join(os.path.dirname(__file__), "config.json")))
 
 bot = commands.Bot(command_prefix= "==")
-
+slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
+guild_ids = [521379436424331265]
 
 @tasks.loop(minutes=30)
 async def check_inventory():
@@ -39,10 +44,69 @@ async def on_message(message):
             user = await bot.fetch_user(int(i[3]))
             await user.send(f"You are missing {', '.join(neededMods)} from {name}")
 
-    if(message.content.startswith("==gv")):
-        user = config.load()["usersToCheck"][0]
-        print("---------")
-        print(gv.getVendorData(user[0],user[1],user[2]))
+
+
+
+
+@slash.slash(name="registerme",
+             description="Registers you with the bot",
+             options=[
+               create_option(
+                 name="url",
+                 description="Enter the braytech URL",
+                 option_type=3,
+                 required=False
+               )
+             ], guild_ids=guild_ids)
+
+async def registerme(ctx, url: str):
+    membershipType = url.split("/")[3]
+    destinyMembershipId = url.split("/")[4]
+    characterId = url.split("/")[5]
+    discordID = str(ctx.author.id)
+
+    users = config.load()["usersToCheck"]
+    newUser = [membershipType, destinyMembershipId, characterId, discordID]
+    isNewUser = True
+
+    for i, j in enumerate(users):
+        if(newUser[3] == j[3]):
+            users[i] = newUser
+            isNewUser = False
+    if(isNewUser):
+        users.append(newUser)
+
+    await ctx.send("Added you to the list!")
+
+    newConfig = config.load()
+
+    newConfig["usersToCheck"] = users
+
+    config.save(newConfig)
+
+@slash.slash(name="unregisterme",
+             description="Registers you with the bot",guild_ids=guild_ids)
+
+async def unregisterme(ctx):
+
+
+    await ctx.send(content="Removed you from the list!")
+
+    discordID = str(ctx.author.id)
+    users = config.load()["usersToCheck"]
+
+    for i, j in enumerate(users):
+        if(users[i][3] == discordID):
+            users.pop(i)
+
+    newConfig = config.load()
+
+    newConfig["usersToCheck"] = users
+
+    config.save(newConfig)
+
+
+
 
 
 bot.run(config.load()["token"])
